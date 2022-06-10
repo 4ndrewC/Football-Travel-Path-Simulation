9407a5f10782ab0cb7c394ec104658822937081e
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2022-06-10 23:12:10 +0800

    Update graphing.py

--- graphing.py
+++ graphing.py
@@ -10,14 +10,11 @@ y = []
 z = []
 for i in range(309):
     row = list(map(float, fin.readline().split()))
-    x.append(row[0])
-    y.append(row[1])
-    z.append(row[2])
+    x.append(row[1])
+    y.append(row[2])
+    z.append(row[3])
 
 graph = plt.plot(x,y,z)
 
-# ax.axes.set_xlim3d(left=0, right=100) 
-# ax.axes.set_ylim3d(bottom=-1000, top=0) 
-# ax.axes.set_zlim3d(bottom=0, top=-1000) 
 ax.plot(x, y, z, 'blue')
 plt.show()
