c5d2440b26d1dc0119b5d4596c6d41895fab0c70
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2022-06-10 23:11:52 +0800

    Update 2dgraph.py

--- 2dgraph.py
+++ 2dgraph.py
@@ -1,12 +1,30 @@
 from matplotlib import pyplot as plt
+import xlwt
+from xlwt import Workbook
+
+wb = Workbook()
+  
+sheet1 = wb.add_sheet('Sheet 1')
 
 fin = open('coordinates.out', 'r')
 x = []
 y = []
 for i in range(309):
     row = list(map(float, fin.readline().split()))
-    x.append(row[0])
-    y.append(row[1])
+    # the index that is reffered to in row[index] corresponds to the column number in coordinates.out
+    # in this case, it is graphing a y vs. z 2D graph
+    x.append(row[2])
+    y.append(row[3])
+plt.xlabel("y position")
+plt.ylabel("z position")
+
+# the commented out code segment is for writing into the excel files to retrieve equations for the curves
+
+# for i in range(309):
+#     sheet1.write(i+1, 0, x[i])
+# for i in range(309):
+#     sheet1.write(i+1, 1, y[i])
+# wb.save('zgraphs.xls')
 
 plt.plot(x,y)
 plt.show()
