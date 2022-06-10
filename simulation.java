e7360126a3973024cc2f16fe6c3038f9bd998628
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2022-06-10 23:11:16 +0800

    Update simulation.java

--- simulation.java
+++ simulation.java
@@ -1,6 +1,16 @@
+/*Flow: at every increment, there is a new drag force that depends on the previously incremented velocity
+use drag force to calculate new angular velocity, which takes in the previously incremented angular velocity as the parameter
+replace old angular vel with new angular vel
+use new angular velocity of x and z to calculate yForce
+Force functions also require previously incremented velocity
+use new forces to calculate new velocities
+reset prev velocities to new velocities*/
+
 import java.io.*;
 
 public class simulation{
+
+    //constants
     static double cd = 0.5;
     static double cl = 0.25;
     static double p = 1.225;
@@ -9,42 +19,46 @@ public class simulation{
     //radius is in meters
     static double r = 0.11;
     static double g = 9.8;
+
+    //area
     static double a = Math.PI*Math.pow(r, 2);
 
-    static double vxi = 12;
-    static double vyi = 3;
-    static double vzi = 18;
-
-    static double dt = 0.01;
-
-    static double wxi = 0.2;
-    static double wyi = 0.2;
-    static double wzi = 0.2;
+    //initial velocity constants
+    static Double vxi = 12.0;
+    static Double vyi = 3.0;
+    static Double vzi = 18.0;
+    static Double dt = 0.01;
+    static Double wxi = 0.2;
+    static Double wyi = 0.2;
+    static Double wzi = 0.2;
     
+    //current position of the ball on each axis
     static Double x = 0.0;
     static Double y = 0.0;
     static Double z = 0.0;
 
     public static void main(String[] args) throws IOException{
         FileWriter fw = new FileWriter("coordinates.out");
-        //find velocity due to force caused by velocity from previous increment, multiply it by the time to get 
-        //travel distanes in a single increment on the x, y, and z axis
         
-        double prevx = vxi;
-        double prevy = vyi;
-        double prevz = vzi;
-        double prevwx = wxi;
-        double prevwy = wyi;
-        double prevwz = wzi;
+        Double time = 0.0;
+        Double prevx = vxi;
+        Double prevy = vyi;
+        Double prevz = vzi;
+        Double prevwx = wxi;
+        Double prevwy = wyi;
+        Double prevwz = wzi;
 
         // double test = 0;
-        fw.write(x.toString() + " " + y.toString() + " " + z.toString() + "\n");
+        fw.write(time + " " + x.toString() + " " + y.toString() + " " + z.toString() + "\n");
         System.out.println(a);
+        double max = 0;
         while(z>=0){
+            time += dt;
+            max = Math.max(y, max);
             x += prevx*dt;
             y += prevy*dt;
             z += prevz*dt;
-            fw.write(x.toString() + " " + y.toString() + " " + z.toString() + "\n");
+            fw.write(time + " " + x.toString() + " " + y.toString() + " " + z.toString() + "\n");
             prevy = yvel(prevx, prevz, prevy, dt, prevwx, prevwz, prevwy);
             prevx = xvel(prevx, dt);
             prevz = zvel(prevz, dt);
@@ -52,8 +66,8 @@ public class simulation{
             prevwy = angvy(prevwy, dt);
             prevwx = angvx(prevwx, dt);
             prevwz = angvz(prevwz, dt);
-            // test+=dt;
         }
+        
         fw.close();
     }
 
@@ -70,8 +84,6 @@ public class simulation{
     public static double yforce(double vx, double vz, double v, double t, double prevwx, double prevwz, double prevwy){
         double wz = angvz(prevwz, t);
         double wx = angvx(prevwx, t);
-        // double wy = angvy(prevwy, t);
-        //(1/2) * p * A * Math.pow(v, 2) * (cl*(wz*vx-wx*)) - cl;
         double ret = 0.5 * p * a * Math.pow(v, 2) * (cl*(wz*vx-wx*vz)) - cd;
         return ret;
     }
@@ -111,10 +123,3 @@ public class simulation{
 }
 
 
-//Flow: at every increment, there is a new drag force that depends on the previously incremented velocity
-//use drag force to calculate new angular velocity, which takes in the previously incremented angular velocity as the parameter
-//replace old angular vel with new angular vel
-//use new angular velocity of x and z to calculate yForce
-//Force functions also require previously incremented velocity
-//use new forces to calculate new velocities
-//reset prev velocities to new velocities
